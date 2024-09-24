f86527f421c107f26ed67eb0e300db120e23fb05
kfir <hkfirs@hotmail.com>
2024-09-25 00:38:45 +0300

    Removed docker and other browser options will re-add in later phase

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -12,141 +12,89 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 
+# Global driver instance
 driver = None
 
+# Set up logging for the test framework
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
-# This method is for creation of --browser_type option when running for cmd (can be set under pycharm config also)
 def pytest_addoption(parser):
+    """
+    Adds command-line options for browser type and environment to pytest.
+
+    Args:
+        parser: The parser object to add command-line options.
+    """
     parser.addoption(
-        "--browser_type", action="store", default="chrome", help="my option: chrome,firefox or edge"
+        "--browser_type", action="store", default="chrome", help="Specify the browser: chrome, firefox, or edge"
     )
     parser.addoption(
-        "--run_env", action="store", default="local", help="my option: local or docker"
+        "--run_env", action="store", default="local", help="Specify the environment: local or docker"
     )
 
 
 @pytest.fixture(scope='class')
 def setup_browser(request):
-    # global driver
+    """
+    Pytest fixture that sets up the browser driver before each test class is run.
+
+    Args:
+        request: Pytest fixture request object to access test-specific data.
+
+    Yields:
+        Yields the initialized browser driver and quits the driver after the tests finish.
+    """
+    global driver
     browser_name = request.config.getoption('browser_type')
     run_env = request.config.getoption('run_env')
 
-    # Common browser setup
-    def setup_chrome():
-        chrome_options = ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--start-maximized')
-        chrome_options.add_argument('--disable-software-rasterizer')
-        chrome_options.add_argument('--ignore-certificate-errors')
-        # Updated Chrome path to run in the container
-        # chrome_path = r"C:\Program Files\Chrome\chrome-win64\chrome.exe"
-        # chrome_options.binary_location = chrome_path
-        # return chrome_options
-
-    def setup_firefox():
-        firefox_options = FirefoxOptions()
-        # Uncomment the line below if you want to run Firefox in headless mode
-        firefox_options.headless = True
-        return firefox_options
-
-    def setup_edge():
-        edge_options = EdgeOptions()
-        edge_options.add_argument('--start-maximized')
-        edge_options.add_argument('--ignore-certificate-errors')
-        return edge_options
-
-    # Local environment setup
+    # Set up browser based on the environment and browser type
     if run_env == 'local':
         if browser_name.lower() == 'chrome':
             print('Chrome - Local')
             chrome_options = ChromeOptions()
-            #chrome_options.add_argument('--no-sandbox')
-            #chrome_options.add_argument('--headless=new')
-            #chrome_options.add_argument('--disable-dev-shm-usage')
+            # Uncomment below line to run tests in headless mode
+            # chrome_options.add_argument('--headless=new')
             chrome_options.add_argument('--start-maximized')
-            #chrome_options.add_argument('--ignore-certificate-errors')
-            #chrome_options = setup_chrome()
-            #chrome_path = r"C:\Program Files\Chrome\chrome-win64\chrome.exe"
-            #chrome_options.binary_location = chrome_path
-            # set the user-agent back to chrome.
-            user_agent = 'Chrome/129.0.6668.59'
-            chrome_options.add_argument(f'user-agent={user_agent}')
-            # TODO: Modify for Windows or Linux based on your platform
-            #service = ChromeService(r"C:\Program Files\Chrome\ChromeDriver\chromedriver-win64\chromedriver.exe")
-            # TODO FOR LOCAL WINDOWS:
+            chrome_options.add_argument('--ignore-certificate-errors')
             service = ChromeService(r"browserdriver\\chromedriver128.exe")
             driver = webdriver.Chrome(service=service, options=chrome_options)
 
-
-        elif browser_name.lower() == 'firefox':
-            print('FireFox - Local')
-            firefox_options = setup_firefox()
-            # TODO: Modify for Windows or Linux based on your platform
-            service = FirefoxService(r"browserdriver\\geckodriver.exe")  # Windows path example
-            driver = webdriver.Firefox(service=service, options=firefox_options)
-
-        elif browser_name.lower() == 'edge':
-            print('Edge - Local')
-            edge_options = setup_edge()
-            # TODO: Modify for Windows or Linux based on your platform
-            service = EdgeService(r"browserdriver\\msedgedriver.exe")  # Windows path example
-            driver = webdriver.Edge(service=service, options=edge_options)
-
+        # Add cases for firefox and edge here if needed
         else:
             raise ValueError("You should choose a browser between chrome, firefox, or edge")
 
-    # Docker/Grid environment setup
-    elif run_env == 'docker':
-        if browser_name.lower() == 'chrome':
-            print('Chrome - Docker')
-            chrome_options = setup_chrome()
-            driver = webdriver.Remote(
-                command_executor="http://selenium-hub:4444/wd/hub",
-                options=chrome_options
-            )
-
-        elif browser_name.lower() == 'firefox':
-            print('FireFox - Docker')
-            firefox_options = setup_firefox()
-            driver = webdriver.Remote(
-                command_executor="http://selenium-hub:4444/wd/hub",
-                options=firefox_options
-            )
-
-        else:
-            raise ValueError("Only Chrome and Firefox are supported in Docker")
-
-    else:
-        raise ValueError("You should choose the environment between local and docker")
-
-    # Implicit wait for MAX TIMEOUT X sec
+    # Set an implicit wait for elements
     driver.implicitly_wait(4)
 
-    # Open the desired website (can be parameterized later)
+    # Open the specified website (can be parameterized later)
     driver.get("https://www.saucedemo.com/")
 
-    # Attach the driver to the class
+    # Attach the driver to the class for use in test methods
     request.cls.driver = driver
 
+    # Yield driver to the test class, and quit after all tests finish
     yield
-    time.sleep(4)  # for demo purposes
+    time.sleep(4)  # Pause for demo purposes
     driver.quit()
 
 
 def pytest_configure(config):
-    # Generate timestamp
+    """
+    Configures the test report settings, such as setting up the directory and file name for HTML reports.
+
+    Args:
+        config: Pytest config object to modify runtime settings.
+    """
+    # Generate a timestamp for the report file
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     report_dir = 'Reports'
     os.makedirs(report_dir, exist_ok=True)
     report_filename = f"{report_dir}/report_{timestamp}.html"
 
-    # Store the report filename in the pytest configuration
+    # Store the HTML report path in the pytest configuration
     config.option.htmlpath = report_filename
 
 
@@ -168,18 +116,20 @@ def _capture_screenshot(file_path):
         This will save the screenshot as 'test.png', or 'test_1.png' if 'test.png' already exists.
     """
     try:
-        # Ensure the directory exists
+        # Ensure the directory for screenshots exists
         directory = os.path.dirname(file_path)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        # Ensure not to overwrite existing file , adding index
+
+        # Avoid overwriting existing screenshot files by appending an index to the filename
         if os.path.exists(file_path):
             file_name, file_extension = os.path.splitext(file_path)
             index = 1
             while os.path.exists(file_path):
                 file_path = f"{file_name}_{index}{file_extension}"
                 index += 1
-        # Capture the screenshot
+
+        # Capture the screenshot using the global WebDriver instance
         driver.get_screenshot_as_file(file_path)
         logger.info(f"Screenshot saved to {file_path}")
 
@@ -190,29 +140,36 @@ def _capture_screenshot(file_path):
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
     """
-    Extends the PyTest Plugin to take and embed a screenshot in the HTML report whenever a test fails.
+    Pytest hook that captures and embeds a screenshot into the HTML report if a test fails.
+
+    Args:
+        item: The pytest test item being reported.
     """
+    # Access the HTML plugin for report generation
     pytest_html = item.config.pluginmanager.getplugin('html')
+
+    # Run the test and get the result
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
 
     logger.info(f"Report status: {report.when} - {report.outcome}")
 
+    # If the test failed or was skipped, capture a screenshot
     if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
+            # Set the screenshot file path
             file_name = report.nodeid.replace("::", "_") + ".png"
-            # set path for failure screenshot to be located in Screenshots folder
             file_path = file_name.replace('Tests', 'Screenshots')
 
             logger.info(f"Capturing screenshot for {report.nodeid} to {file_path}")
 
-            # Capture the screenshot to the Screenshot directory (path is relative to the framework pj
+            # Capture the screenshot and save it to the Reports folder
             screen_shots_path = 'Reports/' + file_path
             _capture_screenshot(screen_shots_path)
 
-            # Embed screenshot in HTML report  (path is relative to the HTML report)
+            # Embed the screenshot in the HTML report (path relative to the HTML report)
             if file_name:
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
                        'onclick="window.open(this.src)" align="right"/></div>' % file_path
