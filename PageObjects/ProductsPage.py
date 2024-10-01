9af7911b955a50fdef592337389e1f7269716c24
kfir <hkfirs@hotmail.com>
2024-10-01 18:11:20 +0300

    Remove some commented code Added locator for Remove buttons

--- PageObjects/ProductsPage.py
+++ PageObjects/ProductsPage.py
@@ -1,20 +1,22 @@
 import time
+
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from Utils.BaseClass import BaseClass
 from PageObjects.CartPage import CartPage
 from PageObjects.SideBar import SideBar
+from PageObjects.SingleProductPage import SingleProductPage
 
 
 class ProductsPage(BaseClass):
     """Page object model for the Products Page, handling product interactions and cart management."""
 
     # Locators for elements on the Products Page
-    l_side_menu_button = (By.ID, "react-burger-menu-btn")
+    l_side_menu_button = (By.ID, "react-burger-menu-btn")  # TODO move it to baseclass
     l_title = (By.CSS_SELECTOR, ".title")
     l_all_page_products_names = (By.CSS_SELECTOR, ".inventory_item_name")
     l_add_to_cart_buttons = (By.XPATH, "//button[text() = 'Add to cart']")
-    # l_shop_cart = (By.CLASS_NAME, "shopping_cart_link")  # TODO move to base class
-    #l_cart_icon_number_of_products = (By.CSS_SELECTOR, ".shopping_cart_badge") # TODO move to base class
+    l_remove_from_cart_buttons = (By.XPATH, "//button[text() = 'Remove']")
 
     def __init__(self, driver):
         """
@@ -24,7 +26,7 @@ class ProductsPage(BaseClass):
         """
         super().__init__()
         self._driver = driver
-        # Initialize Sidebar for sidebar actions
+        # Initialize Sidebar for sidebar-related actions
         self._sidebar = SideBar(self._driver)
 
     def log_out(self):
@@ -43,7 +45,7 @@ class ProductsPage(BaseClass):
         from PageObjects.HomePage import HomePage
         return HomePage(self._driver)
 
-    def reset_application_state(self, sidebar):
+    def reset_application_state(self):  # Todo move to BaseClass
         """
         Resets the application state through the sidebar.
 
@@ -52,7 +54,7 @@ class ProductsPage(BaseClass):
         :param sidebar: Sidebar object used to reset the application state and log out.
         """
         self._driver.find_element(*ProductsPage.l_side_menu_button).click()
-        sidebar.reset_app()
+        self._sidebar.reset_app_and_logout()
 
     def get_page_title(self) -> str:
         """
@@ -89,7 +91,7 @@ class ProductsPage(BaseClass):
         """
 
         for product in product_name:
-            product = product.replace('-', ' ')  # if the product name has - replace it with space
+            product = product.replace('-', ' ')  # if the product name has - replace it with a space
             add_to_cart_buttons = self._driver.find_elements(*ProductsPage.l_add_to_cart_buttons)
             for button in add_to_cart_buttons:
                 # Extract product name from the button's ID, ignoring the "add-to-cart" prefix
@@ -99,23 +101,13 @@ class ProductsPage(BaseClass):
                     button.click()
                     break
 
-    # def get_number_of_products_from_cart_icon(self):
-    #     """
-    #     Returns the number of products displayed in the cart icon.
-    #
-    #     :return: The product count as an int.
-    #     """
-    #     products_count = int(self._driver.find_element(*ProductsPage.l_cart_icon_number_of_products).text)
-    #     return products_count
-    #
-    # def click_shopping_cart(self):  # TODO move to baseclass  !!!!
-    #     """
-    #     Navigates to the shopping cart by clicking the cart icon.
-    #
-    #     :return: CartPage object representing the cart page.
-    #     """
-    #
-    #     self.c(CartPage.l_all_page_products_names)
-    #
-    #     self._driver.find_element(*ProductsPage.l_shop_cart).click()
-    #     return CartPage(self._driver)
+    def check_if_product_added(self, product_name):
+        # return if element is found
+        l_remove_btn_by_name = f"remove-{product_name.lower().replace(' ', '-')}"
+        return self._driver.find_element(By.ID, l_remove_btn_by_name)
+
+    def click_product_by_name(self, product_name):
+
+        # instead of looping through page products name use the direct link approach
+        self._driver.find_element(By.LINK_TEXT, product_name).click()
+        return SingleProductPage(self._driver)
