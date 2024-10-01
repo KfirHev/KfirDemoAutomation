dacc7c259b632e0e6aaaf9f814e1b90fcfc74a3e
kfir <hkfirs@hotmail.com>
2024-10-01 18:12:46 +0300

    Remove some commented code Added locator for back to products page Added locator for product price (single product page)

--- PageObjects/SingleProductPage.py
+++ PageObjects/SingleProductPage.py
@@ -1,7 +1,6 @@
 import time
 from selenium.webdriver.common.by import By
 from Utils.BaseClass import BaseClass
-from PageObjects.CartPage import CartPage
 from PageObjects.SideBar import SideBar
 
 
@@ -9,11 +8,11 @@ class SingleProductPage(BaseClass):
     """Page object model for the Products Page, handling product interactions and cart management."""
 
     # Locators for elements on the Products Page
-    l_side_menu_button = (By.ID, "react-burger-menu-btn")
+    l_side_menu_button = (By.ID, "react-burger-menu-btn")  # TODO move it to baseclass
     l_product_name = (By.CSS_SELECTOR, ".inventory_details_name")
     l_add_to_cart_button = (By.XPATH, "//button[text() = 'Add to cart']")
-    # l_shop_cart = (By.CLASS_NAME, "shopping_cart_link") # moved to base class
-    # l_cart_icon_number_of_products = (By.CSS_SELECTOR, ".shopping_cart_badge") # moved to base class
+    l_back_to_products = (By.ID, "back-to-products")
+    l_product_price = (By.CSS_SELECTOR, ".inventory_details_price")
 
     def __init__(self, driver):
         """
@@ -26,3 +25,17 @@ class SingleProductPage(BaseClass):
 
         # Initialize Sidebar for sidebar actions
         self._sidebar = SideBar(self._driver)
+
+    def get_product_name(self):
+        return self._driver.find_element(*SingleProductPage.l_product_name).text
+
+    def get_product_price(self):
+        return self._driver.find_element(*SingleProductPage.l_product_price).text
+
+    def add_product_to_cart(self):
+        self._driver.find_element(*SingleProductPage.l_add_to_cart_button).click()
+
+    def back_to_product_page_click(self):
+        self._driver.find_element(*SingleProductPage.l_back_to_products).click()
+        from PageObjects.ProductsPage import ProductsPage
+        return ProductsPage(self._driver)
