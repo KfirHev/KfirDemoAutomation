c88a075fdbfa723f656eb916e5d7572d1ca376f9
kfir <hkfirs@hotmail.com>
2024-10-01 18:13:41 +0300

    Added some data for testing single product page , two different products

---
+++ TestData/SingleProductPageData.py
@@ -0,0 +1,15 @@
+
+class SingleProductPageData:
+
+    test_single_product = [
+        {
+            'product_name': 'Sauce Labs Onesie',
+            'product_price': 7.99
+        },
+
+        {
+            'product_name': 'Sauce Labs Bike Light',
+            'product_price': 9.99
+        }
+
+    ]
