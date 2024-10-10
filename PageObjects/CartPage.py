17049408197dfd2a6b8dc43f1e32cf16a625a200
kfir <hkfirs@hotmail.com>
2024-10-10 11:49:29 +0300

    Removed redundant locators and sidebar initialization (moved to base class)

--- PageObjects/CartPage.py
+++ PageObjects/CartPage.py
@@ -7,9 +7,9 @@ class CartPage(BaseClass):
     """Page object model for the Cart Page."""
 
     # Locators for elements on the Cart Page
-    l_side_menu_button = (By.ID, "react-burger-menu-btn")  # TODO move it to baseclass
     l_all_page_products_names = (By.CSS_SELECTOR, ".inventory_item_name")
     l_continue_shopping_button = (By.ID, "continue-shopping")
+    l_remove_from_cart_buttons = (By.XPATH, "//button[text() = 'Remove']")
 
     def __init__(self, driver):
         """
@@ -19,8 +19,6 @@ class CartPage(BaseClass):
         """
         super().__init__()
         self._driver = driver
-        # Initialize Sidebar for sidebar-related actions
-        self._sidebar = SideBar(self._driver)
 
     def get_page_products_name(self):
         """
@@ -36,37 +34,14 @@ class CartPage(BaseClass):
         from PageObjects.ProductsPage import ProductsPage
         return ProductsPage(self._driver)
 
-    def log_out(self):
+    def remove_all_products_from_the_cart(self):  # TODO move locator and method to base class ? count usage
         """
-        Logs out of the application using the sidebar's log out method.
-
-        This method first verifies the side menu button is displayed, then
-        clicks the button to open the menu and logs out via the Sidebar object.
-
-        :return: HomePage object representing the user being redirected to the home page.
+        Removes all visible products from the cart by clicking on each "Remove" button.
         """
-        # Verify the side menu button is visible before proceeding
-        self.verify_element_displayed(CartPage.l_side_menu_button)
+        remove_from_cart_buttons = self._driver.find_elements(*CartPage.l_remove_from_cart_buttons)
+        for button in remove_from_cart_buttons:
+            button.click()
 
-        # Open the sidebar by clicking the side menu button
-        self._driver.find_element(*CartPage.l_side_menu_button).click()
 
-        # Perform the logout using the sidebar
-        self._sidebar.log_out()
 
-        # Redirect back to the home page after logging out
-        from PageObjects.HomePage import HomePage
-        return HomePage(self._driver)
 
-    def reset_application_state(self): # Todo move to BaseClass
-        """
-        Resets the application state by opening the sidebar and clearing the session.
-
-        This method clicks the side menu button, then calls the reset function from the
-        Sidebar class to reset the application state and log out.
-        """
-        # Open the sidebar by clicking the side menu button
-        self._driver.find_element(*CartPage.l_side_menu_button).click()
-
-        # Use the sidebar object to reset the app and log out
-        self._sidebar.reset_app_and_logout()
