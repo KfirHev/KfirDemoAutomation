c54d43c4fa8cfbd06ee5621c91e0f15b55ab72f7
kfir <hkfirs@hotmail.com>
2024-10-13 11:09:01 +0300

    Added get product details methods ,price,details and image.

--- PageObjects/SingleProductPage.py
+++ PageObjects/SingleProductPage.py
@@ -1,7 +1,6 @@
 import time
 from selenium.webdriver.common.by import By
 from Utils.BaseClass import BaseClass
-from PageObjects.SideBar import SideBar
 
 
 class SingleProductPage(BaseClass):
@@ -9,10 +8,12 @@ class SingleProductPage(BaseClass):
 
     # Locators for elements on the Products Page
     l_product_name = (By.CSS_SELECTOR, ".inventory_details_name")
-    l_add_to_cart_button = (By.XPATH, "//button[text() = 'Add to cart']")
-    l_remove_from_cart_button = (By.XPATH, "//button[text() = 'Remove']") # Todo move to base class
-    l_back_to_products = (By.ID, "back-to-products")
     l_product_price = (By.CSS_SELECTOR, ".inventory_details_price")
+    l_product_details = (By.CSS_SELECTOR, ".inventory_details_desc")
+    l_product_image = (By.CSS_SELECTOR, ".inventory_details_img")
+    l_add_to_cart_button = (By.XPATH, "//button[text() = 'Add to cart']")
+    l_remove_from_cart_button = (By.XPATH, "//button[text() = 'Remove']")  # Todo move to base class
+    l_back_to_products = (By.ID, "back-to-products")
 
     def __init__(self, driver):
         """
@@ -23,22 +24,36 @@ class SingleProductPage(BaseClass):
         super().__init__()
         self._driver = driver
 
-        # Initialize Sidebar for sidebar actions
-        self._sidebar = SideBar(self._driver)
-
     def get_product_name(self):
-        return self._driver.find_element(*SingleProductPage.l_product_name).text
+        """Retrieve the product name."""
+        return self._driver.find_element(*self.l_product_name).text
 
     def get_product_price(self):
-        return self._driver.find_element(*SingleProductPage.l_product_price).text
+        """Retrieve the product price."""
+        return self._driver.find_element(*self.l_product_price).text
+
+    def get_product_details(self):
+        """Retrieve the product description."""
+        return self._driver.find_element(*self.l_product_details).text
+
+    def get_product_image(self):
+        """Retrieve the product image URL."""
+        return self._driver.find_element(*self.l_product_image).get_attribute('src')
 
     def add_product_to_cart(self):
-        self._driver.find_element(*SingleProductPage.l_add_to_cart_button).click()
+        """Add the product to the cart."""
+        self._driver.find_element(*self.l_add_to_cart_button).click()
 
     def remove_product_from_cart(self):
-        self._driver.find_element(*SingleProductPage.l_remove_from_cart_button).click()
+        """Remove the product from the cart."""
+        self._driver.find_element(*self.l_remove_from_cart_button).click()
 
     def back_to_product_page_click(self):
-        self._driver.find_element(*SingleProductPage.l_back_to_products).click()
+        """
+        Navigate back to the products page.
+
+        :return: ProductsPage instance.
+        """
+        self._driver.find_element(*self.l_back_to_products).click()
         from PageObjects.ProductsPage import ProductsPage
         return ProductsPage(self._driver)
