7230edcd6e4b34b4f64e98a8eef9d1f1b111ec14
kfir <hkfirs@hotmail.com>
2024-10-21 11:41:20 +0300

    Added data for an expected failure case

--- TestData/SingleProductPageData.py
+++ TestData/SingleProductPageData.py
@@ -11,5 +11,14 @@ class SingleProductPageData:
             'product_name': 'Sauce Labs Bike Light',
             'product_price': 9.99
         }
+        ]
+
+    test_error_single_product = [
+        {
+            'product_name': 'Sauce Labs Backpack'
+        },
+        {
+            'product_name': 'Sauce Labs Bolt T-Shirt'
+        }
 
     ]
