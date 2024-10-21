31e7fa95530fff1ebc07bc5cbdd2b7aaefdc9742
kfir <hkfirs@hotmail.com>
2024-10-21 11:40:21 +0300

    Added and fixed docstrings to page methods

--- PageObjects/ProductsPage.py
+++ PageObjects/ProductsPage.py
@@ -1,14 +1,18 @@
 import time
-
 from selenium.webdriver.common.by import By
-
 from PageObjects.CheckOutInfoPage import CheckOutInfoPage
 from Utils.BaseClass import BaseClass
 from PageObjects.SingleProductPage import SingleProductPage
 
 
 class ProductsPage(BaseClass):
-    """Page object model for the Products Page, handling product interactions and cart management."""
+    """
+    Page object model for the Products Page, handling product interactions and cart management.
+
+    This class includes methods to interact with products, add or remove items from the cart,
+    sort products, and navigate to the checkout page. Each method is designed to be reusable
+    for various tests involving the Products Page.
+    """
 
     # Locators for elements on the Products Page
     l_all_products = (By.CSS_SELECTOR, ".inventory_item")
@@ -22,7 +26,7 @@ class ProductsPage(BaseClass):
 
     def __init__(self, driver):
         """
-        Initializes the ProductsPage object with the WebDriver instance and a SideBar instance.
+        Initializes the ProductsPage object
 
         :param driver: WebDriver instance for interacting with the page elements.
         """
@@ -38,7 +42,12 @@ class ProductsPage(BaseClass):
         return self.get_products_name(ProductsPage.l_all_page_products_names)
 
     def get_product_info(self, product_name):
+        """
+        Retrieves the details, price, and image source for a given product.
 
+        :param product_name: The name of the product to retrieve information for.
+        :return: A tuple containing product details (description), price, and image URL.
+        """
         products = self._driver.find_elements(*ProductsPage.l_all_products)
         for product in products:
             if product_name in product.text:
@@ -50,12 +59,30 @@ class ProductsPage(BaseClass):
                 return product_details, product_price, image_src
 
     def get_product_details(self, product_name):
+        """
+        Fetches the product details for a given product.
+
+        :param product_name: The name of the product to get details for.
+        :return: The product description as a string.
+        """
         return self.get_product_info(product_name)[0]
 
     def get_product_price(self, product_name):
+        """
+        Fetches the price for a given product.
+
+        :param product_name: The name of the product to get the price for.
+        :return: The product price as a string.
+        """
         return self.get_product_info(product_name)[1]
 
     def get_product_image(self, product_name):
+        """
+        Fetches the image URL for a given product.
+
+        :param product_name: The name of the product to get the image URL for.
+        :return: The URL of the product image as a string.
+        """
         return self.get_product_info(product_name)[2]
 
     def add_all_products_to_cart(self):
@@ -75,24 +102,29 @@ class ProductsPage(BaseClass):
             button.click()
 
     def add_product_to_cart(self, product_name):
-        """Add product to the cart by its name."""
+        """
+        Adds a specific product to the cart based on the product name.
+
+        :param product_name: The name of the product to add to the cart.
+        """
         self._add_or_remove_products(product_name, action="add")
 
     def remove_product_from_cart(self, product_name):
-        """Remove product from the cart by its name."""
+        """
+        Removes a specific product from the cart based on the product name.
+
+        :param product_name: The name of the product to remove from the cart.
+        """
         self._add_or_remove_products(product_name, action="remove")
 
     def _add_or_remove_products(self, product_name, action):
         """
-        Adds or removes product(s) from the cart based on the action.
+        Adds or removes product(s) from the cart based on the specified action.
 
-        Identifies the button dynamically by the product name and performs the specified action
-        (either 'add' or 'remove').
-
-        :param product_name: Single product or list of product names to be added or removed.
-        :param action: Action to perform - 'add' or 'remove'.
+        :param product_name: The name or list of names of products to add or remove.
+        :param action: The action to perform, either "add" or "remove".
         """
-        if type(product_name) is str:  # handle cases where single name is sent
+        if type(product_name) is str:  # handle cases where a single name is sent
             product_name = [product_name]
         for product in product_name:
             product_formatted = product.lower().replace(' ', '-')
@@ -104,33 +136,31 @@ class ProductsPage(BaseClass):
 
     def check_if_product_added(self, product_name):
         """
-        Checks if a specific product is added to the cart by looking for the remove button.
+        Checks if a specific product has been added to the cart by looking for the remove button.
 
         :param product_name: The name of the product to check for.
-        :return: The WebElement representing the remove button if found, otherwise raises an exception.
+        :return: The WebElement representing the remove button if found.
+        :raises NoSuchElementException: If the remove button for the product is not found.
         """
-        # Construct the ID for the remove button by formatting the product name
         l_remove_btn_by_name = f"remove-{product_name.lower().replace(' ', '-')}"
         return self._driver.find_element(By.ID, l_remove_btn_by_name)
 
     def click_product_by_name(self, product_name):
         """
-        Clicks on a product link by its name to navigate to the product's detail page.
+        Clicks on a product by its name to navigate to its detail page.
 
         :param product_name: The name of the product to click on.
-        :return: An instance of the SingleProductPage class after the click action.
+        :return: An instance of the SingleProductPage class.
         """
-        # Click the product link directly using the link text
         self._driver.find_element(By.LINK_TEXT, product_name).click()
         return SingleProductPage(self._driver)
 
     def sort_products_by(self, sorting_option):
         """
-        Selects a sorting option from the dropdown to sort the products displayed on the page.
+        Sorts the products displayed on the page by a given sorting option.
 
-        :param sorting_option: The sorting option to be selected (e.g., 'Name (A to Z)', 'Price (low to high)').
+        :param sorting_option: The sorting option to select (e.g., 'Name (A to Z)', 'Price (low to high)').
         """
-        # Click on the sort dropdown and select the desired option
         self._driver.find_element(*ProductsPage.l_sort_dropdown).click()
         self._driver.find_element(By.XPATH, f"//option[text()='{sorting_option}']").click()
 
@@ -139,13 +169,11 @@ class ProductsPage(BaseClass):
         Verifies that the products are sorted correctly based on the selected sorting option.
 
         :param sorting_option: The sorting option used for verification.
-        :return: True if the sorting is correct, False otherwise.
+        :return: True if the products are sorted correctly, False otherwise.
         """
-        # Retrieve the product names and prices from the page
         products_names = self._driver.find_elements(*ProductsPage.l_all_page_products_names)
         products_prices = self._driver.find_elements(*ProductsPage.l_all_page_products_prices)
 
-        # Check sorting correctness based on the selected option
         if sorting_option == 'Name (Z to A)':
             product_list = [pr.text for pr in products_names]
             return product_list == sorted(product_list, reverse=True)
@@ -163,5 +191,10 @@ class ProductsPage(BaseClass):
             return product_list == sorted(product_list, reverse=True)
 
     def checkout(self):
+        """
+        Clicks the checkout button to proceed to the checkout information page.
+
+        :return: An instance of the CheckOutInfoPage class.
+        """
         self._driver.find_element(*ProductsPage.l_checkout_button).click()
         return CheckOutInfoPage(self._driver)
