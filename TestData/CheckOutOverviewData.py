2422077b9ce46db878b187904dcbc09f94d9b65f
kfir <hkfirs@hotmail.com>
2024-10-21 11:36:44 +0300

    Added CheckoutOverviewPage for prices calculations methods and locators

---
+++ TestData/CheckOutOverviewData.py
@@ -0,0 +1,20 @@
+class CheckoutOverviewData:
+
+    test_products = [
+        {
+            'product_names': [
+                'Sauce Labs Backpack',
+                'Sauce Labs Fleece Jacket',
+                'Test.allTheThings() T-Shirt (Red)',
+                'Sauce Labs Bolt T-Shirt',
+            ]
+        },
+        {
+            'product_names': [
+                'Sauce Labs Bolt T-Shirt',
+                'Sauce Labs Fleece Jacket',
+                'Sauce Labs Onesie',
+            ]
+        }
+    ]
+
