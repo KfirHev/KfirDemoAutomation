af43e8f96aa69feb478237523cd9fccd008461f2
kfir <hkfirs@hotmail.com>
2024-10-28 16:10:41 +0200

    Added success message locator and get message function

--- PageObjects/CheckoutCompletePage.py
+++ PageObjects/CheckoutCompletePage.py
@@ -5,14 +5,21 @@ from Utils.BaseClass import BaseClass
 class CheckoutCompletePage(BaseClass):
     # Locators for elements on the CheckoutOverView Page
 
-    #l_cart_items = (By.CSS_SELECTOR, '.cart_item')
-
+    l_success_msg = (By.TAG_NAME, 'h2')
 
     def __init__(self, driver):
         """
-        Initializes the CartPage object with the WebDriver instance.
+        Initializes the CheckoutCompletePage object with the WebDriver instance.
 
         :param driver: WebDriver instance for interacting with the page.
         """
         super().__init__()
         self._driver = driver
+
+    def get_success_message(self):
+        """
+        Fetches the error message displayed after a failed submit attempt.
+
+        :return: The login error message as a string.
+        """
+        return self._driver.find_element(*CheckoutCompletePage.l_success_msg).text
